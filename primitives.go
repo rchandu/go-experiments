b920c38063a3b0946bc763757e96336180ebbf04
Rama Chandu <rama.chandu57@gmail.com>
2022-11-02 21:57:35 +0530

    Minor updates to primitive testing file

--- primitives.go
+++ primitives.go
@@ -3,14 +3,19 @@ package main
 import "fmt"
 
 func main() {
+	// Printing on console is done through fmt package.
+	fmt.Println("hello there!")
 
 	// String concatenation works too
-	fmt.Println("go" + "lang")
+	fmt.Println("strings" + " are " + "concatenated")
 
+	// Regular numeric math operations
 	fmt.Println("1+1 =", 1+1)
 	fmt.Println("7.0/3.0 =", 7.0/3.0)
 
-	fmt.Println(true && false)
-	fmt.Println(true || false)
-	fmt.Println(!true)
+	// Basic boolean operations
+	fmt.Println("true && false", true && false)
+	fmt.Println("true || false", true || false)
+	fmt.Println("!true", !true)
+	fmt.Println("!!true", !!true)
 }
